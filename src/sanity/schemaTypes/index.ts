b30b65a1c399534e297fae7a7c7b567d9bfe59ac
amber officil <amberofficial380@gmail.com>
2025-02-07 17:07:04 +0500

    hackthon7day

---
+++ src/sanity/schemaTypes/index.ts
@@ -0,0 +1,13 @@
+
+import { type SchemaTypeDefinition } from 'sanity'
+import product from './product'
+
+
+
+
+
+
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+    types: [product],
+}
